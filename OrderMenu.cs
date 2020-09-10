14f18881e059c5fa1da9fa69c07617d251611a2a
mattboy2001 <57643755+mattboy2001@users.noreply.github.com>
2020-09-10 22:24:06 +0100

    Menus

---
+++ OrderMenu.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OrderMenu : MonoBehaviour
+{
+
+
+    public Sprite squareSprite;
+
+    public Sprite triangleSprite;
+
+    public Sprite circleSprite;
+
+
+    Sprite currentSprite;
+
+
+    public Image Image1;
+
+    public Image Image2;
+
+    public Image Image3;
+
+
+
+    int firstOrder;
+
+    int secondOrder;
+
+    int thirdOrder;
+
+    public void FirstButton()
+    {
+
+         if (Image1.sprite == squareSprite)
+        {
+            Image1.sprite = circleSprite;
+        }
+        else if (Image1.sprite == circleSprite)
+        {
+            Image1.sprite = triangleSprite;
+        }
+        else if (Image1.sprite == triangleSprite)
+        {
+            Image1.sprite = squareSprite;
+        }
+
+
+
+    }
+
+    public void SecondButton()
+    {
+           if (Image2.sprite ==squareSprite)
+        {
+            Image2.sprite = circleSprite;
+        }
+        else if (Image2.sprite == circleSprite)
+        {
+            Image2.sprite = triangleSprite;
+        }
+        else if (Image2.sprite == triangleSprite)
+        {
+            Image2.sprite = squareSprite;
+        }
+    }
+
+    public void ThirdButton()
+    {
+               if (Image3.sprite ==squareSprite)
+        {
+            Image2.sprite = circleSprite;
+        }
+        else if (Image3.sprite == circleSprite)
+        {
+            Image3.sprite = triangleSprite;
+        }
+        else if (Image3.sprite == triangleSprite)
+        {
+            Image3.sprite = squareSprite;
+        }
+    }
+}
